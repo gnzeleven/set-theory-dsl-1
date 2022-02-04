a088a2e2388e0053f8fdb34688ed60e76110aa5c
gnzeleven <vsav4796@gmail.com>
2022-02-03 20:35:34 -0600

    Create project skeleton

---
+++ src/main/scala/Driver.scala
@@ -0,0 +1,16 @@
+package org.cs474.setdsl
+
+/**
+ * Factory for [[Driver]] instances
+ * The workflow starts here
+ * */
+object Driver {
+
+  /** Main Method - Triggers the gRPC or REST server based on command line input
+   * @param args : Array[String] - command line input
+   * @return Unit
+   */
+  def main(args: Array[String]) = {
+    println("Works")
+  }
+}
---
+++ src/main/scala/Utils/ObtainConfigReference.scala
@@ -0,0 +1,6 @@
+package org.cs474.setdsl
+package Utils
+
+class ObtainConfigReference {
+
+}
