3782176f6932295feadd318d7da2a57364e09bd9
gnzeleven <vsav4796@gmail.com>
2022-02-03 21:01:24 -0600

    Set up template

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -1,16 +1,22 @@
 package org.cs474.setdsl
 
+import Utils.{CreateLogger, ObtainConfigReference}
+
 /**
  * Factory for [[Driver]] instances
  * The workflow starts here
  * */
 object Driver {
-
+  val logger = CreateLogger(this.getClass)
   /** Main Method - Triggers the gRPC or REST server based on command line input
    * @param args : Array[String] - command line input
    * @return Unit
    */
   def main(args: Array[String]) = {
-    println("Works")
+    val config = ObtainConfigReference("DSL") match {
+      case Some(value) => value
+      case None => throw new RuntimeException("Cannot obtain a reference to the config data.")
+    }
+    logger.info("works")
   }
 }
--- src/main/scala/Utils/ObtainConfigReference.scala
+++ src/main/scala/Utils/ObtainConfigReference.scala
@@ -1,6 +1,25 @@
 package org.cs474.setdsl
 package Utils
 
-class ObtainConfigReference {
+import com.typesafe.config.{Config, ConfigFactory}
+import scala.util.{Failure, Success, Try}
 
+object ObtainConfigReference {
+
+  private val config = ConfigFactory.load()
+  private val logger = CreateLogger(classOf[this.type])
+
+  private def ValidateConfig(confEntry: String): Boolean = {
+    Try(config.getConfig(confEntry)) match {
+      case Failure(ex) => {
+        logger.error(s"Failed to retrieve config entry ${confEntry} for reason ${ex}")
+        false
+      }
+      case Success(_) => true
+    }
+  }
+
+  def apply(confEntry:String): Option[Config] = {
+    if ValidateConfig(confEntry) then Some(config) else None
+  }
 }
